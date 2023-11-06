ec9e8359e3f416f6e324b607752b1f6c352951c9
ryan horowitz <ryan.w.horowitz@gmail.com>
2023-11-06 13:03:15 -0500

    enabled all access CORS frontend and backend are hooked up!

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 from flask import Flask, Response
+from flask_cors import CORS
 import cv2
 
 app = Flask(__name__)
+CORS(app)
 video_capture = cv2.VideoCapture(0)
 # 0 represents the default camera (your MacBook Air's webcam)
 streaming = False
